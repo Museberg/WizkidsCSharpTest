7f45afa0ca8fb9388d5d1e37390bd91b8f182264
maus <rasmus@falk-jensen.dk>
2023-01-30 10:06:16 +0100

    Minor cleanup

--- WordPredictionFrontend/src/app/app.component.spec.ts
+++ WordPredictionFrontend/src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'WordPredictionFrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WordPredictionFrontend');
   });
 
   it('should render title', () => {
